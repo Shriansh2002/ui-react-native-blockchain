dae097e540bf80b2fbc141fe1b1cc36e01d3f640
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-04-15 10:20:32 +0530

    Part-2

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
@@ -5,12 +6,15 @@ import { useFonts } from 'expo-font';
 import Home from './screens/Home';
 import Details from './screens/Details';
 
-const Stack = createStackNavigator();
-
 const theme = {
 	...DefaultTheme,
-	colors: { ...DefaultTheme.colors, background: 'transparent' },
+	colors: {
+		...DefaultTheme.colors,
+		background: 'transparent',
+	},
 };
+
+const Stack = createStackNavigator();
 
 const App = () => {
 	const [loaded] = useFonts({
@@ -26,11 +30,13 @@ const App = () => {
 	return (
 		<NavigationContainer theme={theme}>
 			<Stack.Navigator
-				screenOptions={{ headerShown: false }}
+				screenOptions={{
+					headerShown: false,
+				}}
 				initialRouteName="Home"
 			>
-				<Stack.Screen name="home" component={Home} />
-				<Stack.Screen name="details" component={Details} />
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Details" component={Details} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
---
+++ components/Button.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export const CircleButton = () => {
+	return (
+		<View>
+			<Text>Button</Text>
+		</View>
+	);
+};
+
+export const RectButton = () => {
+	return (
+		<View>
+			<Text>Button</Text>
+		</View>
+	);
+};
